b031a34af57c47c918969ea67c1f2acf7adba2d4
Stefan <cveticstefancpl@gmail.com>
2020-06-13 15:28:17 +0200

    Add location activity and its design

---
+++ app/src/main/java/com/example/rmaapp/AddLocationActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rmaapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+class AddLocationActivity: AppCompatActivity(R.layout.activity_add_location) {
+}
--- app/src/main/java/com/example/rmaapp/MainActivity.kt
+++ app/src/main/java/com/example/rmaapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.rmaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initCardListeners()
+    }
+
+    private fun initCardListeners() {
+        addLocationCard.setOnClickListener {
+            val intent = Intent(this, AddLocationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
