ff616df786030775fca1877b52eb289960edec6a
Stefan <cveticstefancpl@gmail.com>
2020-06-14 23:28:25 +0200

    Parcelize SavedLocation

--- app/src/main/java/com/example/rmaapp/presentation/model/SavedLocation.kt
+++ app/src/main/java/com/example/rmaapp/presentation/model/SavedLocation.kt
@@ -1,5 +1,8 @@
 package com.example.rmaapp.presentation.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
-data class SavedLocation(val title: String, val note: String, val dateCreated: Date = Date(), val id: Int = 0)
+@Parcelize
+data class SavedLocation(val title: String, val note: String, val dateCreated: Date = Date(), val id: Int = 0) : Parcelable
