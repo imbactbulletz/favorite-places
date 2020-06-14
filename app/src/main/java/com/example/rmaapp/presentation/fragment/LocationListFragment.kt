50f6f6357f7d8ab547cfde687753acc3b27eea07
Stefan <cveticstefancpl@gmail.com>
2020-06-14 19:34:29 +0200

    Initialize recycler

--- app/src/main/java/com/example/rmaapp/presentation/fragment/LocationListFragment.kt
+++ app/src/main/java/com/example/rmaapp/presentation/fragment/LocationListFragment.kt
@@ -1,9 +1,24 @@
 package com.example.rmaapp.presentation.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rmaapp.R
+import com.example.rmaapp.presentation.adapter.recycler.SavedLocationListAdapter
 import kotlinx.android.synthetic.main.activity_add_location.view.*
+import kotlinx.android.synthetic.main.fragment_location_list.*
 
 class LocationListFragment: Fragment(R.layout.fragment_location_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        locationRecyclerView.adapter = SavedLocationListAdapter()
+        locationRecyclerView.layoutManager = LinearLayoutManager(activity)
+    }
 
 }
