8e133c03ae524fbdf3308bac042458e7b748d7b5
Stefan <cveticstefancpl@gmail.com>
2020-06-15 19:52:44 +0200

    Implement sorting of saved locations by date

--- app/src/main/java/com/example/rmaapp/presentation/fragment/LocationListFragment.kt
+++ app/src/main/java/com/example/rmaapp/presentation/fragment/LocationListFragment.kt
@@ -1,8 +1,11 @@
 package com.example.rmaapp.presentation.fragment
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,14 +20,56 @@ import org.koin.android.viewmodel.ext.android.viewModel
 
 class LocationListFragment : Fragment(R.layout.fragment_location_list) {
 
+    companion object {
+        private const val INITIAL_POSITION = 0F
+    }
+
     private val viewModel: LocationListContract.ViewModel by viewModel<LocationListFragmentViewModel>()
 
     private lateinit var recyclerAdapter: SavedLocationListAdapter
 
+    private var sortButtonRotationAngle = INITIAL_POSITION
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initButtonListeners()
         initRecyclerView()
         initObservers()
+    }
+
+    private fun initButtonListeners() {
+        sortButton.setOnClickListener {
+            sortAdapterDataSetByDate(sortButtonRotationAngle == INITIAL_POSITION)
+            rotateSortButton()
+        }
+    }
+
+    private fun sortAdapterDataSetByDate(ascending: Boolean) {
+        val adapterList = recyclerAdapter.currentList.toMutableList()
+        if(recyclerAdapter.currentList.isNotEmpty()) {
+            when(ascending) {
+                true -> adapterList.sortBy { it.dateCreated }
+                false -> adapterList.sortByDescending { it.dateCreated }
+            }
+
+            recyclerAdapter.submitList(adapterList) { locationRecyclerView.scrollToPosition(0) }
+        }
+    }
+
+
+    private fun rotateSortButton() {
+
+        sortButtonRotationAngle += 180F
+        val sortButtonAnimator = ObjectAnimator.ofFloat(sortButton, "rotation", sortButtonRotationAngle)
+        sortButtonAnimator.duration = 500
+        sortButtonAnimator.interpolator = AccelerateDecelerateInterpolator()
+        sortButtonAnimator.start()
+        sortButtonAnimator.doOnEnd {
+            if(sortButtonRotationAngle == 360F) {
+                sortButton.rotation = 0F
+                sortButtonRotationAngle = INITIAL_POSITION
+            }
+        }
     }
 
     private fun initRecyclerView() {
@@ -35,7 +80,7 @@ class LocationListFragment : Fragment(R.layout.fragment_location_list) {
 
     private fun initObservers() {
         viewModel.getAllSavedLocations().observe(viewLifecycleOwner) { locations ->
-            recyclerAdapter.submitList(locations)
+            recyclerAdapter.submitList(locations.sortedByDescending { it.dateCreated })
         }
     }
 
