cd3aba89ca9229be0fb6f6f2cd7f991912770257
Stefan <cveticstefancpl@gmail.com>
2020-06-15 22:02:53 +0200

    Navigate map camera to the most recently saved location

--- app/src/main/java/com/example/rmaapp/presentation/fragment/MapHistoryFragment.kt
+++ app/src/main/java/com/example/rmaapp/presentation/fragment/MapHistoryFragment.kt
@@ -1,19 +1,26 @@
 package com.example.rmaapp.presentation.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.example.rmaapp.presentation.contract.MapHistoryContract
 import com.example.rmaapp.presentation.viewmodel.MapHistoryFragmentViewModel
 import com.google.android.gms.maps.SupportMapFragment
 import org.koin.android.viewmodel.ext.android.viewModel
 import androidx.lifecycle.observe
+import com.example.rmaapp.presentation.activity.AddLocationActivity
 import com.example.rmaapp.presentation.model.SavedLocation
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.clustering.ClusterManager
 
 class MapHistoryFragment: SupportMapFragment() {
+
+    companion object {
+        private const val ZOOM_LEVEL_FACTOR = 14F
+    }
 
     private val viewModel: MapHistoryContract.ViewModel by viewModel<MapHistoryFragmentViewModel>()
 
@@ -42,13 +49,19 @@ class MapHistoryFragment: SupportMapFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.findAll().observe(viewLifecycleOwner) { savedLocations ->
+            val sortedSavedLocations = savedLocations.sortedByDescending { it.dateCreated  }
+
             viewModel.isMapReady().observe(viewLifecycleOwner) { isMapReady ->
                 if (isMapReady) {
                     clusterManager.clearItems()
-                    clusterManager.addItems(savedLocations)
+                    clusterManager.addItems(sortedSavedLocations)
+                    navigateMapCameraToPosition(sortedSavedLocations[0].position)
                 }
             }
         }
     }
 
+    private fun navigateMapCameraToPosition(position: LatLng) {
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(position, ZOOM_LEVEL_FACTOR))
+    }
 }
