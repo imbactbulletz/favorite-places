2b8a6239b9efa4fd95da563f7af83a66a987f119
Stefan <cveticstefancpl@gmail.com>
2020-06-24 23:11:17 +0200

    Change GlobalScope with viewModelScope

--- app/src/main/java/com/example/rmaapp/presentation/viewmodel/AddLocationViewModel.kt
+++ app/src/main/java/com/example/rmaapp/presentation/viewmodel/AddLocationViewModel.kt
@@ -3,6 +3,7 @@ package com.example.rmaapp.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.rmaapp.data.local.repository.SavedLocationRepository
 import com.example.rmaapp.presentation.contract.AddLocationContract
 import com.example.rmaapp.presentation.model.SavedLocation
@@ -17,7 +18,7 @@ class AddLocationViewModel(private val savedLocationRepository: SavedLocationRep
     private val currentPosition = MutableLiveData<LatLng>()
 
     override fun save(savedLocation: SavedLocation) {
-        GlobalScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(Dispatchers.IO) {
             savedLocationRepository.save(savedLocation)
         }
     }
--- app/src/main/java/com/example/rmaapp/presentation/viewmodel/EditActivityViewModel.kt
+++ app/src/main/java/com/example/rmaapp/presentation/viewmodel/EditActivityViewModel.kt
@@ -2,6 +2,7 @@ package com.example.rmaapp.presentation.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.rmaapp.data.local.repository.SavedLocationRepository
 import com.example.rmaapp.presentation.contract.EditContract
 import com.example.rmaapp.presentation.model.SavedLocation
@@ -12,7 +13,7 @@ import kotlinx.coroutines.launch
 class EditActivityViewModel(private val repository: SavedLocationRepository): ViewModel(), EditContract.ViewModel {
 
     override fun edit(savedLocation: SavedLocation) {
-        GlobalScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(Dispatchers.IO) {
             repository.edit(savedLocation)
         }
     }
